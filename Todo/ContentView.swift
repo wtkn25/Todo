b9ba6576f00343088bad514ecca71bf4ecde9fc7
wtkn25 <atrastar72@gmail.com>
2020-07-08 23:38:54 +0900

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by ken on 2020/07/08.
+//  Copyright © 2020 ken. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
