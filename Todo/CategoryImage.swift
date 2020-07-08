0e15acc5ba2917e679076766e606483e3286742a
wtkn25 <atrastar72@gmail.com>
2020-07-08 23:59:59 +0900

    first commit (tortise)

---
+++ Todo/CategoryImage.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryImage.swift
+//  Todo
+//
+//  Created by ken on 2020/07/08.
+//  Copyright © 2020 ken. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryImage: View {
+    var body: some View {
+        Image(systemName: "tortoise.fill")
+    }
+}
+
+struct CategoryImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryImage()
+    }
+}
