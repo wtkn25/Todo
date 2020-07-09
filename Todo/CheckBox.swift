19498e52e2de5827f9bd16dc53ddc12016e41293
wtkn25 <atrastar72@gmail.com>
2020-07-10 00:30:49 +0900

    ジェネリックなるものを使ってみた。 チェックボックスが動作しないけど一旦無視することにした。 全体的にわからんし

--- Todo/CheckBox.swift
+++ Todo/CheckBox.swift
@@ -8,21 +8,37 @@
 
 import SwiftUI
 
-struct CheckBox: View {
+struct CheckBox<Label>: View where Label: View{
     @Binding var checked: Bool
+    private var label: ()-> Label
+    
+    public init(checked: Binding<Bool>,
+                @ViewBuilder label:@escaping()-> Label){
+        self._checked = checked
+        self.label = label
+    }
+    
     var body: some View {
-        Image(systemName: checked ? "checkmark.circle":"circle")
-            .onTapGesture {
-                self.checked.toggle()
+        HStack {
+            Image(systemName: checked ? "checkmark.circle":"circle")
+                .onTapGesture {
+                    self.checked.toggle()
             }
+            label()
+        }
     }
 }
 
 struct CheckBox_Previews: PreviewProvider {
     static var previews: some View {
         VStack{
-            CheckBox(checked: .constant(false))
-            CheckBox(checked: .constant(true))
+            CheckBox(checked: .constant(false)) {
+                Text("牛乳を買う")
+            }
+            CheckBox(checked: .constant(true)) {
+                Image(systemName: "hand.thumbsup")
+                Text("牛乳を買う")
+            }
         }
     }
 }
