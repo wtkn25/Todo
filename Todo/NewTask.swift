658e71a1ddb03875ca5b2cad970f30cfdc4d0c75
wtkn25 <hoop.zone.burst@gmail.com>
2020-07-11 23:21:31 +0900

    DatePicker追加

--- Todo/NewTask.swift
+++ Todo/NewTask.swift
@@ -10,10 +10,18 @@ import SwiftUI
 
 struct NewTask: View {
     @State var task: String = ""
+    @State var time: Date = Date()
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
                 TextField("タスクを入力", text: $task)
+                DatePicker(selection: $time, label: { Text("日時")} )
+                Button(action: {}) {
+                    HStack(alignment: .center) {
+                        Image(systemName: "minus.circle.fill")
+                        Text("キャンセル")
+                    }.foregroundColor(.red)
+                }
             }.navigationBarTitle("タスクの追加")
         }
     }
